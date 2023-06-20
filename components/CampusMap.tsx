371c8552739053f31662096de28474492e8abc03
faz919 <faizit9@gmail.com>
2023-06-20 13:35:29 -0500

    added all locations on map

--- components/CampusMap.tsx
+++ components/CampusMap.tsx
@@ -3,7 +3,7 @@ import map from '../public/campus_map.png'
 
 export default function CampusMap() {
     return (
-        <iframe src="https://www.google.com/maps/d/embed?mid=12eIVfNBAOgLdRXQqKjG341JoQBNxGAA&hl=en&ehbc=2E312F" className='iframe'>
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=12eIVfNBAOgLdRXQqKjG341JoQBNxGAA&ehbc=2E312F" className='iframe'>
             Your browser does not support iframes.
         </iframe>
     )
