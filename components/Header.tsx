206e691f5ce80903c4a0f11b87b687bc08dd2a76
faz919 <faizit9@gmail.com>
2023-07-06 12:39:18 -0500

    mac dead

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,12 +19,12 @@ export default function Header({ scrollHandler }) {
             <div className="absolute inset-0 bg-orange-100 mix-blend-multiply" />
           </div>
           <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-            <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl  font-semibold uppercase tracking-wide uiuc-orange">
+            {/* <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl  font-semibold uppercase tracking-wide uiuc-orange">
               HungryUIUC
-            </p>
+            </p> */}
             <h1 className="mt-1 text-center font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
-              <span className="block text-white">Hungry?</span>
-              <span className="block uiuc-orange">We got you.</span>
+              <span className="block text-white drop-shadow-md">Hungry?</span>
+              <span className="block uiuc-orange drop-shadow-md">We got you.</span>
             </h1>
 
             <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,7 +12,7 @@ export default function ProductCard({ product }) {
   return (
     <Link href={`/products/${product.id}`} className="group">
       <div className="option-image">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 duration-500">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 duration-500 group-hover:scale-105 group-hover:drop-shadow-md">
           <Image
             alt="product image"
             src={product.image}
