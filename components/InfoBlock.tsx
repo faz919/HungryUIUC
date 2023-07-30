5e23ca7fe01cc39c291f2fff1c1b2ae50abfa1f5
faz919 <faizit9@gmail.com>
2023-07-30 17:03:30 -0500

    did some stuff

--- components/InfoBlock.tsx
+++ components/InfoBlock.tsx
@@ -9,7 +9,7 @@ export default function InfoBlock({ resource, reverse }) {
     const [isLoading, setLoading] = useState(true);
     return (
         <a href={resource.link} className={cn("w-full bg-gray-200 my-6 h-auto sm:h-72 flex flex-col rounded-lg overflow-hidden shadow-md duration-300 scale-100 hover:scale-105", reverse ? "sm:flex-row-reverse" : "sm:flex-row")}>
-            <div className="aspect-1 h-72 overflow-hidden bg-gray-200 relative">
+            <div className="aspect-1 h-72 overflow-hidden bg-gray-200 relative min-w-fit">
                 <Image
                     alt={resource.id}
                     src={resource.image}
@@ -23,11 +23,11 @@ export default function InfoBlock({ resource, reverse }) {
                     onLoadingComplete={() => setLoading(false)}
                 />
             </div>
-            <div className="p-4">
-                <div className={cn("mt-4 flex items-center font-medium text-gray-900", reverse ? "sm:text-right" : "sm:text-left")}>
-                    <h3>{resource.name}</h3>
+            <div className="p-6">
+                <div className={"flex items-center font-medium text-gray-900"}>
+                    <h3 className={cn("text-xl font-bold", reverse ? "sm:text-right" : "sm:text-left")}>{resource.name}</h3>
                 </div>
-                <p className="mt-1 text-sm italic text-gray-500">
+                <p className="mt-1 text-md italic text-gray-500">
                     {resource.shortDescription}
                 </p>  
             </div>
