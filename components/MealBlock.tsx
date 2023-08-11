5cd4bc571c396129cf072c957d26dc5d11d54c94
faz919 <faizit9@gmail.com>
2023-08-11 13:01:20 -0500

    yo

---
+++ components/MealBlock.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { useState } from "react";
+
+function cn(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function MealBlock({ meal }) {
+    const [isLoading, setLoading] = useState(true);
+    return (
+        <a href={meal.link} className={cn("group w-full bg-gray-200 my-6 h-auto lg:h-72 flex flex-col rounded-lg overflow-hidden shadow-md duration-300 scale-100 hover:scale-105 lg:flex-row")}>
+            <div className="aspect-1 h-72 overflow-hidden bg-gray-200 relative min-w-fit">
+                <Image
+                    alt={meal.id}
+                    src={meal.image}
+                    fill
+                    className={cn(
+                        'object-cover duration-300 ease-in-out opacity-80 group-hover:opacity-100',
+                        isLoading
+                            ? 'blur-2xl grayscale scale-110'
+                            : 'blur-0 grayscale-0 scale-100'
+                    )}
+                    onLoadingComplete={() => setLoading(false)}
+                />
+            </div>
+            <div className="p-6">
+                <div className={"flex items-center font-medium text-gray-900"}>
+                    <h3 className={cn("text-xl font-bold sm:text-left")}>{meal.name}</h3>
+                </div>
+                <p className="mt-1 text-md italic text-gray-500">
+                    {meal.shortDescription}
+                </p>
+                <p className="mt-1 italic underline">
+                    Learn More
+                </p>
+            </div>
+        </a>
+    )
+}
--- pages/products/weekly-schedule.tsx
+++ pages/products/weekly-schedule.tsx
@@ -1,13 +1,31 @@
+import moment from 'moment-timezone'
 import Header from '../../components/Header'
 import Sponsors from '../../components/Sponsors'
+import schedule from '../../data/weekly-schedule.json'
+import MealBlock from '../../components/MealBlock'
 
 export default function Product() {
-
+  moment.tz.setDefault("America/Chicago")
   return (
     <>
-      <Header scrollHandler={() => {}} mainPageHeader={false} title={"Weekly Schedule"}/>
+      <Header scrollHandler={() => { }} mainPageHeader={false} title={"Weekly Schedule"} />
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-
+        {Array.from(Array(7).keys()).map((item, idx) => {
+          const day = moment().add(idx, "days").format("dddd")
+          return (
+            <>
+              <div className="mt-5 w-full h-4 border-b-2 border-solid border-black text-center">
+                <span className="bg-white px-4 py-0 text-lg">
+                  {idx == 0 ? "Today" : day}
+                </span>
+              </div>
+              {schedule.filter((meal) => meal.dayOfWeek == day).map((meal) => (
+                <MealBlock meal={meal}/>
+              ))}
+            </>
+          )
+        })
+        }
       </div>
       <Sponsors />
     </>
